5c2c0b89030d3d790e109f01cb777a6d477fc6bb
Jaxon OpTiC <jaxongpitcock@gmail.com>
2024-08-07 16:05:39 -0400

    Fix of old buggy code - replaced tasks with invokes, modernized UI.

--- PackOptimizer/Form1.Designer.cs
+++ PackOptimizer/Form1.Designer.cs
@@ -28,29 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             richTextBox1 = new RichTextBox();
             button3 = new Button();
             button2 = new Button();
-            menuStrip1 = new MenuStrip();
-            optionsToolStripMenuItem = new ToolStripMenuItem();
-            expirementalFeaturesToolStripMenuItem = new ToolStripMenuItem();
             button4 = new Button();
-            label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             asyncExtractCheckBox = new CheckBox();
             label5 = new Label();
-            menuStrip1.SuspendLayout();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.BackColor = Color.White;
+            button1.BackColor = Color.FromArgb(8, 21, 31);
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(155, 238);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 332);
             button1.Name = "button1";
             button1.Size = new Size(100, 29);
             button1.TabIndex = 0;
@@ -60,29 +54,34 @@
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new Point(12, 273);
+            richTextBox1.BackColor = Color.FromArgb(8, 21, 31);
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.ForeColor = Color.White;
+            richTextBox1.Location = new Point(12, 53);
             richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(561, 123);
+            richTextBox1.Size = new Size(561, 273);
             richTextBox1.TabIndex = 1;
             richTextBox1.Text = "";
             // 
             // button3
             // 
-            button3.BackColor = Color.White;
+            button3.BackColor = Color.FromArgb(8, 21, 31);
             button3.FlatStyle = FlatStyle.Flat;
-            button3.Location = new Point(367, 238);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(224, 332);
             button3.Name = "button3";
             button3.Size = new Size(100, 29);
             button3.TabIndex = 5;
-            button3.Text = "Pack";
+            button3.Text = "Backup";
             button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // button2
             // 
-            button2.BackColor = Color.White;
+            button2.BackColor = Color.FromArgb(8, 21, 31);
             button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(261, 238);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(118, 332);
             button2.Name = "button2";
             button2.Size = new Size(100, 29);
             button2.TabIndex = 6;
@@ -90,35 +89,12 @@
             button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
-            // menuStrip1
-            // 
-            menuStrip1.Items.AddRange(new ToolStripItem[] { optionsToolStripMenuItem });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(585, 24);
-            menuStrip1.TabIndex = 8;
-            menuStrip1.Text = "menuStrip1";
-            // 
-            // optionsToolStripMenuItem
-            // 
-            optionsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { expirementalFeaturesToolStripMenuItem });
-            optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            optionsToolStripMenuItem.Size = new Size(61, 20);
-            optionsToolStripMenuItem.Text = "Options";
-            // 
-            // expirementalFeaturesToolStripMenuItem
-            // 
-            expirementalFeaturesToolStripMenuItem.Name = "expirementalFeaturesToolStripMenuItem";
-            expirementalFeaturesToolStripMenuItem.Size = new Size(190, 22);
-            expirementalFeaturesToolStripMenuItem.Text = "Expiremental Features";
-            expirementalFeaturesToolStripMenuItem.Click += expirementalFeaturesToolStripMenuItem_Click;
-            // 
             // button4
             // 
-            button4.BackColor = Color.Silver;
-            button4.Enabled = false;
+            button4.BackColor = Color.FromArgb(8, 21, 31);
             button4.FlatStyle = FlatStyle.Flat;
-            button4.Location = new Point(473, 238);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(330, 332);
             button4.Name = "button4";
             button4.Size = new Size(100, 29);
             button4.TabIndex = 9;
@@ -126,59 +102,26 @@
             button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(12, 249);
-            label1.Name = "label1";
-            label1.Size = new Size(92, 21);
-            label1.TabIndex = 10;
-            label1.Text = "Log Viewer";
-            // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(12, 31);
+            label2.Location = new Point(12, 9);
             label2.Name = "label2";
-            label2.Size = new Size(59, 21);
+            label2.Size = new Size(52, 21);
             label2.TabIndex = 11;
-            label2.Text = "gch2.0";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(12, 52);
-            label3.Name = "label3";
-            label3.Size = new Size(176, 21);
-            label3.TabIndex = 12;
-            label3.Text = "created by yoursorrow";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.ForeColor = Color.White;
-            label4.Location = new Point(3, 82);
-            label4.Name = "label4";
-            label4.Size = new Size(585, 120);
-            label4.TabIndex = 13;
-            label4.Text = resources.GetString("label4.Text");
+            label2.Text = "GCH3";
             // 
             // asyncExtractCheckBox
             // 
             asyncExtractCheckBox.AutoSize = true;
             asyncExtractCheckBox.ForeColor = Color.White;
-            asyncExtractCheckBox.Location = new Point(261, 213);
+            asyncExtractCheckBox.Location = new Point(12, 367);
             asyncExtractCheckBox.Name = "asyncExtractCheckBox";
-            asyncExtractCheckBox.Size = new Size(97, 19);
+            asyncExtractCheckBox.Size = new Size(105, 19);
             asyncExtractCheckBox.TabIndex = 14;
-            asyncExtractCheckBox.Text = "Async Extract";
+            asyncExtractCheckBox.Text = "Async Features";
             asyncExtractCheckBox.UseVisualStyleBackColor = true;
             // 
             // label5
@@ -187,37 +130,42 @@
             label5.Cursor = Cursors.Hand;
             label5.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label5.ForeColor = Color.White;
-            label5.Location = new Point(113, 253);
+            label5.Location = new Point(537, 329);
             label5.Name = "label5";
             label5.Size = new Size(36, 15);
             label5.TabIndex = 15;
             label5.Text = "Clear";
             label5.Click += label5_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(443, 18);
+            label1.TabIndex = 16;
+            label1.Text = "This project is not regularly maintained. Expect massive issues.";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(45, 45, 45);
-            ClientSize = new Size(585, 408);
+            BackColor = Color.FromArgb(11, 32, 48);
+            ClientSize = new Size(585, 393);
+            Controls.Add(label1);
             Controls.Add(label5);
             Controls.Add(asyncExtractCheckBox);
-            Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(label2);
-            Controls.Add(label1);
             Controls.Add(button4);
             Controls.Add(button2);
             Controls.Add(button3);
             Controls.Add(richTextBox1);
             Controls.Add(button1);
-            Controls.Add(menuStrip1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
-            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Load += Form1_Load;
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -228,15 +176,10 @@
         private RichTextBox richTextBox1;
         private Button button3;
         private Button button2;
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem optionsToolStripMenuItem;
-        private ToolStripMenuItem expirementalFeaturesToolStripMenuItem;
         private Button button4;
-        private Label label1;
         private Label label2;
-        private Label label3;
-        private Label label4;
         private CheckBox asyncExtractCheckBox;
         private Label label5;
+        private Label label1;
     }
 }
--- PackOptimizer/Form1.cs
+++ PackOptimizer/Form1.cs
@@ -70,7 +70,6 @@ namespace PackOptimizer
 
                     string newDirectoryPath = Path.Combine(exeLocation, directoryName);
 
-                    // Create the extraction directory if it doesn't exist
                     if (!Directory.Exists(newDirectoryPath))
                     {
                         Directory.CreateDirectory(newDirectoryPath);
@@ -81,7 +80,6 @@ namespace PackOptimizer
                         string extractedFolder = Path.GetFileNameWithoutExtension(gmaFile);
                         string addonFolderPath = Path.Combine(newDirectoryPath, extractedFolder);
 
-                        // Create the addon folder within the "extracted" directory
                         Directory.CreateDirectory(addonFolderPath);
 
                         string addonFileName = Path.GetFileName(gmaFile);
@@ -117,10 +115,8 @@ namespace PackOptimizer
                             await Task.Run(() => process.WaitForExit());
                         }
 
-                        // Create the addon.json file within the extracted folder
                         string addonJsonPath = Path.Combine(addonFolderPath, "addon.json");
 
-                        // Construct the addon.json content
                         string addonJsonContent = @"{
                 ""title"": ""gch Extracted Addon"",
                 ""type"": ""tool"",
@@ -155,7 +151,6 @@ namespace PackOptimizer
             string extractionDirectory = Path.Combine(workingDirectory, "extracted");
             string compressedDirectory = Path.Combine(workingDirectory, "compressed");
 
-            // Create the 'compressed' directory if it doesn't exist
             if (!Directory.Exists(compressedDirectory))
             {
                 Directory.CreateDirectory(compressedDirectory);
@@ -179,7 +174,7 @@ namespace PackOptimizer
                     CreateNoWindow = true,
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
-                    WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory // Set the working directory to the application's base directory
+                    WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory 
                 };
 
                 using (Process process = new Process())
@@ -239,15 +234,12 @@ namespace PackOptimizer
         #region Experimental
 
         /* 
-         * This function is HIGHLY experimental and should not be used for production of content packs yet. 
-         * Please understand that you should always make backups of your extracted .gmas using the Pack button before you Create your pack.
-         * This feature is not entirely supported and should be considered not working properly until then.
+         * This function is now fully supported by GCH3.0, works much better with invokes, less crashes.
          */
         private async void CreateContentPack()
         {
             string workingDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-            // Create the "bin" folder if it doesn't exist
             string binFolderPath = Path.Combine(workingDirectory, "bin");
             if (!Directory.Exists(binFolderPath))
             {
@@ -255,28 +247,26 @@ namespace PackOptimizer
             }
 
             string sourceDirectory = Path.Combine(workingDirectory, "release");
-            string targetFilePath = Path.Combine(binFolderPath, "gchcontentpack.gma"); // Update target file path
+            string targetFilePath = Path.Combine(binFolderPath, "gchcontentpack.gma"); 
 
-            // Remove the target file if it already exists
             if (File.Exists(targetFilePath))
             {
                 File.Delete(targetFilePath);
             }
 
-            // Check if addon.json exists, and create one if it doesn't
             string addonJsonPath = Path.Combine(sourceDirectory, "addon.json");
             if (!File.Exists(addonJsonPath))
             {
                 string addonJsonContent = @"{
-            ""title"": ""gch Content Pack"",
-            ""type"": ""tool"",
-            ""ignore"": [""*.psd""],
-            ""description"": ""This content pack was created using gch."",
-            ""author_name"": ""gch"",
-            ""author_email"": ""gch@example.com"",
-            ""author_website"": ""https://gcpo-website.com"",
-            ""version"": ""1.0""
-        }";
+        ""title"": ""gch Content Pack"",
+        ""type"": ""tool"",
+        ""ignore"": [""*.psd""],
+        ""description"": ""This content pack was created using gch."",
+        ""author_name"": ""gch"",
+        ""author_email"": ""gch@example.com"",
+        ""author_website"": ""https://gcpo-website.com"",
+        ""version"": ""1.0""
+    }";
 
                 File.WriteAllText(addonJsonPath, addonJsonContent);
             }
@@ -325,7 +315,10 @@ namespace PackOptimizer
 
                 if (process.ExitCode == 0)
                 {
-                    MessageBox.Show("Addon packing completed. The merged addon is stored in the 'bin' folder as 'gchcontentpack.gma'.", "Packing Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Invoke((Action)(() =>
+                    {
+                        MessageBox.Show("Addon packing completed. The merged addon is stored in the 'bin' folder as 'gchcontentpack.gma'.", "Packing Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }));
                 }
                 else
                 {
@@ -336,11 +329,15 @@ namespace PackOptimizer
                         errorMessage += Environment.NewLine + Environment.NewLine + "Error Message:" + Environment.NewLine + errorBuilder.ToString();
                     }
 
-                    richTextBox1.Text = richTextBox1.Text + (errorMessage);
+                    Invoke((Action)(() =>
+                    {
+                        richTextBox1.Text = richTextBox1.Text + errorMessage;
+                    }));
                 }
             }
         }
-    
+
+
 
         // SPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED!
         private async void MergeAddons()
@@ -349,7 +346,6 @@ namespace PackOptimizer
             string sourceDirectory = Path.Combine(workingDirectory, "extracted");
             string targetDirectory = Path.Combine(workingDirectory, "release");
 
-            // Create the target directory if it doesn't exist
             if (!Directory.Exists(targetDirectory))
             {
                 Directory.CreateDirectory(targetDirectory);
@@ -366,20 +362,16 @@ namespace PackOptimizer
 
         private async Task MergeAddonsAsync(string[] addonDirectories, string targetDirectory)
         {
-            // Dictionary to keep track of filenames and their occurrences
             Dictionary<string, int> filenameOccurrences = new Dictionary<string, int>();
 
-            // Concurrently merge the addons using multiple tasks
             await Task.WhenAll(addonDirectories.Select(addonDirectory => Task.Run(() =>
             {
                 string addonName = Path.GetFileName(addonDirectory);
 
-                // Get all subdirectories within the addon directory
                 string[] subdirectories = Directory.GetDirectories(addonDirectory, "*", SearchOption.AllDirectories);
 
                 foreach (string subdirectoryPath in subdirectories)
                 {
-                    // Create the corresponding subdirectory in the target directory
                     string relativePath = Path.GetRelativePath(addonDirectory, subdirectoryPath);
                     string targetSubdirectoryPath = Path.Combine(targetDirectory, relativePath);
 
@@ -388,7 +380,6 @@ namespace PackOptimizer
                         Directory.CreateDirectory(targetSubdirectoryPath);
                     }
 
-                    // Copy files within the subdirectory
                     string[] files = Directory.GetFiles(subdirectoryPath);
 
                     foreach (string filePath in files)
@@ -396,14 +387,12 @@ namespace PackOptimizer
                         string fileName = Path.GetFileName(filePath);
                         string targetFilePath = Path.Combine(targetSubdirectoryPath, fileName);
 
-                        // Check if the file already exists in the target subfolder
                         if (File.Exists(targetFilePath))
                         {
                             string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
                             string fileExtension = Path.GetExtension(fileName);
                             int counter = 1;
 
-                            // Generate a new filename by appending a number to the filename
                             while (File.Exists(targetFilePath))
                             {
                                 string newFileName = $"{fileNameWithoutExtension}{counter}{fileExtension}";
@@ -411,13 +400,14 @@ namespace PackOptimizer
                                 counter++;
                             }
 
-                            richTextBox1.AppendText($"Filename conflict found: {fileName}. Renamed to {Path.GetFileName(targetFilePath)}." + Environment.NewLine);
+                            Invoke((Action)(() =>
+                            {
+                                richTextBox1.AppendText($"Filename conflict found: {fileName}. Renamed to {Path.GetFileName(targetFilePath)}." + Environment.NewLine);
+                            }));
                         }
 
-                        // Copy the file to the target subfolder
                         File.Copy(filePath, targetFilePath);
 
-                        // Add the filename to the occurrences dictionary
                         lock (filenameOccurrences)
                         {
                             if (!filenameOccurrences.ContainsKey(fileName))
@@ -433,7 +423,6 @@ namespace PackOptimizer
                 }
             })));
 
-            // Rename files in the target directory if conflicts occurred during merging
             foreach (KeyValuePair<string, int> kvp in filenameOccurrences)
             {
                 string fileName = kvp.Key;
@@ -455,32 +444,32 @@ namespace PackOptimizer
 
                             File.Move(targetFilePath, newTargetFilePath);
 
-                            richTextBox1.AppendText($"Filename conflict found: {fileNameWithoutExtension}{i}{fileExtension}. Renamed to {newFileName}.");
+                            Invoke((Action)(() =>
+                            {
+                                richTextBox1.AppendText($"Filename conflict found: {fileNameWithoutExtension}{i}{fileExtension}. Renamed to {newFileName}.");
+                            }));
                         }
                     }
                 }
             }
 
-            richTextBox1.AppendText("Addon merging completed. Merged files are stored in the 'release' directory.");
             await Task.Run(() => { CreateContentPack(); });
         }
+
         private async Task ExtractGMAsAsync(string[] gmaFiles, string newDirectoryPath, string gmadPath)
         {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-            // Create the extraction directory if it doesn't exist
             if (!Directory.Exists(newDirectoryPath))
             {
                 Directory.CreateDirectory(newDirectoryPath);
             }
 
-            // Concurrently extract the GMA files using multiple tasks
             await Task.WhenAll(gmaFiles.Select(gmaFile => Task.Run(async () =>
             {
                 string extractedFolder = Path.GetFileNameWithoutExtension(gmaFile);
                 string addonFolderPath = Path.Combine(newDirectoryPath, extractedFolder);
 
-                // Create the addon folder within the "extracted" directory
                 Directory.CreateDirectory(addonFolderPath);
 
                 string addonFileName = Path.GetFileName(gmaFile);
@@ -515,10 +504,8 @@ namespace PackOptimizer
                     await Task.Run(() => process.WaitForExit());
                 }
 
-                // Create the addon.json file within the extracted folder
                 string addonJsonPath = Path.Combine(addonFolderPath, "addon.json");
 
-                // Construct the addon.json content
                 string addonJsonContent = @"{
             ""title"": ""gch Extracted Addon"",
             ""type"": ""tool"",
@@ -533,13 +520,15 @@ namespace PackOptimizer
                 File.WriteAllText(addonJsonPath, addonJsonContent);
             })));
             sw.Stop();
-            richTextBox1.AppendText($"Extraction Time: {sw.Elapsed.TotalSeconds} seconds");
-        }
 
+            Invoke((Action)(() =>
+            {
+                richTextBox1.AppendText($"Extraction Time: {sw.Elapsed.TotalSeconds} seconds");
+            }));
+        }
 
         private async Task PackAddonsAsync(string[] addonDirectories, string compressedDirectory, string gmadPath)
         {
-            // Concurrently pack the addon folders using multiple tasks
             await Task.WhenAll(addonDirectories.Select(addonDirectory => Task.Run(async () =>
             {
                 string addonName = Path.GetFileName(addonDirectory);
@@ -571,7 +560,10 @@ namespace PackOptimizer
 
                     if (process.ExitCode != 0)
                     {
-                        richTextBox1.AppendText($"Failed to compress addon folder '{addonName}'." + Environment.NewLine);
+                        Invoke((Action)(() =>
+                        {
+                            richTextBox1.AppendText($"Failed to compress addon folder '{addonName}'." + Environment.NewLine);
+                        }));
                     }
                 }
             })));
@@ -586,24 +578,7 @@ namespace PackOptimizer
 
         #endregion
 
-        #region IDontRememberWhatR4
-        private void expirementalFeaturesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            var confirm = MessageBox.Show("Are you sure you want to enable this? It could break the entire application.", "Confirm", MessageBoxButtons.YesNo);
-            if (confirm == DialogResult.Yes)
-            {
-                expirementalFeaturesToolStripMenuItem.Checked = true;
-                button4.Enabled = true;
-                button4.BackColor = Color.White;
-            }
-            else
-            {
-                return;
-            }
-        }
 
-
-        #endregion
 
         private void label5_Click(object sender, EventArgs e)
         {
